fd361d28fecaa37ea02955de0abdd43d13315d2a
Nikita Petunin <petunin@haulmont.com>
2016-02-15 17:55:22 +0400

    Creation

---
+++ modules/web/src/com/haulmont/styledcheckbox/web/screens/StyledCheckboxExample.java
@@ -0,0 +1,21 @@
+package com.haulmont.styledcheckbox.web.screens;
+
+import com.haulmont.cuba.gui.components.AbstractWindow;
+import com.haulmont.cuba.gui.components.CheckBox;
+import com.haulmont.cuba.web.gui.components.WebComponentsHelper;
+
+import javax.inject.Inject;
+import java.util.Map;
+
+public class StyledCheckboxExample extends AbstractWindow {
+
+    @Inject
+    private CheckBox sampleCheckBox;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        com.vaadin.ui.CheckBox vaadinCheckBox = (com.vaadin.ui.CheckBox) WebComponentsHelper.unwrap(sampleCheckBox);
+        vaadinCheckBox.setCaptionAsHtml(true);
+        vaadinCheckBox.setCaption("<span class=\"onoffswitch-inner\"></span><span class=\"onoffswitch-switch\"></span>");
+    }
+}
